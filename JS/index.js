f24df401a33a23977648f9b49d4435f53492c8a4
Edgar Hovsepyan <edhov91@gmail.com>
2022-07-17 23:46:25 +0400

    added some commit with alert notification

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,11 @@ function changeBorder() {
 }
 
 function notification() {
-    alert("Hi there, here will be more logic using JavaScript soon...");
+    alert("Hi there, this is a simple portfolio just for practice, at this time i started" +
+        " working on my main portfolio with more features, where i also will use " +
+        "React, React-Router, Redux, JS, CSS, HTMl  and so on..." +
+        "if you wanna to give me feedback or just ask something, please fill free to contact with me, i will be happy for that" +
+        "see you soon ;)");
 }
 
 img.addEventListener("mouseover", changeBorder);
